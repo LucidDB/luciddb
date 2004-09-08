24098799a8eeb353affc54efd0d01f099ab50a21
wael <a@b>
2004-09-08 08:51:40 -0800

    FARRAGO: forgot to add
    [git-p4: depot-paths = "//open/dt/dev/": change = 2668]

---
+++ farrago/src/org/eigenbase/sql/fun/SqlLiteralChainOperator.java
@@ -0,0 +1,173 @@
+package org.eigenbase.sql.fun;
+
+import org.eigenbase.sql.*;
+import org.eigenbase.sql.test.SqlTester;
+import org.eigenbase.sql.parser.ParserPosition;
+import org.eigenbase.sql.type.SqlTypeName;
+import org.eigenbase.reltype.RelDataType;
+import org.eigenbase.resource.EigenbaseResource;
+import org.eigenbase.util.NlsString;
+import org.eigenbase.util.BitString;
+
+/**
+ * Internal operator, by which the parser represents a continued string
+ * literal.  The string fragments are SqlLiterals (of the same type),
+ * collected as the operands of an SqlCall with this operator.  The
+ * validator concatenates them into a single RexLiteral.
+ * (For a chain of SqlLiteral.CharStrings, an SqlCollation is attached
+ * only to the head of the chain.)
+ *
+ * @author Marc Berkowitz
+ * @since Sep 7, 2004
+ * @version $Id$
+ */
+public class SqlLiteralChainOperator extends SqlInternalOperator {
+
+    SqlLiteralChainOperator() {
+        super("$LitChain", SqlKind.LitChain, 40, true,
+            // precedence tighter than the * and || operators
+            SqlOperatorTable.useFirstArgType, SqlOperatorTable.useFirstKnownParam, null);
+    }
+
+    // REVIEW mb 8/8/04: Can't use SqlOperator.AllowedArgInference here;
+    // it doesn't handle variadicCountDescriptor operators well.
+    public OperandsCountDescriptor getOperandsCountDescriptor()
+    {
+        return OperandsCountDescriptor.variadicCountDescriptor;
+    }
+
+    // all operands must be the same type
+    private boolean argTypesValid(
+        SqlCall call,
+        SqlValidator validator,
+        SqlValidator.Scope scope)
+    {
+        if (call.operands.length < 2) {
+            return true; // nothing to compare
+        }
+        SqlNode operand = call.operands[0];
+        RelDataType firstType = validator.deriveType(scope, operand);
+        for (int i = 1; i < call.operands.length; i++) {
+            operand = call.operands[i];
+            RelDataType otherType =
+                validator.deriveType(scope, operand);
+            if (!firstType.isSameType(otherType)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    protected void checkArgTypes(
+        SqlCall call,
+        SqlValidator validator,
+        SqlValidator.Scope scope)
+    {
+        if (!argTypesValid(call, validator, scope)) {
+            throw call.newValidationSignatureError(validator, scope);
+        }
+    }
+
+    // Result type is the same as all the args, but its size is the
+    // total size.
+    // REVIEW mb 8/8/04: Possibly this can be achieved by combining
+    // the strategy useFirstArgType with a new transformer.
+    protected RelDataType inferType(
+        SqlValidator validator,
+        SqlValidator.Scope scope,
+        SqlCall call)
+    {
+        // Here we know all the operands have the same type,
+        // which has a size (precision), but not a scale.
+        RelDataType rt =
+            validator.getValidatedNodeType(call.operands[0]);
+        SqlTypeName tname = rt.getSqlTypeName();
+        assert tname.allowsPrecNoScale() : "LitChain has impossible operand type "
+            + tname;
+        int size = rt.getPrecision();
+        for (int i = 1; i < call.operands.length; i++) {
+            rt = validator.getValidatedNodeType(call.operands[i]);
+            size += rt.getPrecision();
+        }
+        return validator.typeFactory.createSqlType(tname, size);
+    }
+
+    public String getAllowedSignatures(String opName)
+    {
+        return opName + "(...)";
+    }
+
+    // per the SQL std, each string fragment must be on a different line
+    void validateCall(
+        SqlCall call,
+        SqlValidator validator)
+    {
+        ParserPosition pos;
+        ParserPosition prevPos = call.operands[0].getParserPosition();
+        for (int i = 1; i < call.operands.length;
+             i++, prevPos = pos) {
+            pos = call.operands[i].getParserPosition();
+            if (pos.getBeginLine() <= prevPos.getBeginLine()) {
+                // FIXME jvs 28-Aug-2004:  i18n
+                throw EigenbaseResource.instance().newValidationError("String literal continued on same line, at "
+                    + pos);
+            }
+        }
+    }
+
+    public void unparse(
+        SqlWriter writer,
+        SqlNode [] rands,
+        int leftPrec,
+        int rightPrec)
+    {
+        SqlCollation collation = null;
+        for (int i = 0; i < rands.length; i++) {
+            if (i > 0) {
+                writer.print(" ");
+            }
+            SqlLiteral rand = (SqlLiteral) rands[i];
+            if (rand instanceof SqlLiteral.CharString) {
+                NlsString nls =
+                    ((SqlLiteral.CharString) rand).getNlsString();
+                if (i == 0) {
+                    collation = nls.getCollation();
+                    writer.print(nls.asSql(true, false)); // print with prefix
+                } else {
+                    writer.print(nls.asSql(false, false)); // print without prefix
+                }
+            } else if (i == 0) {
+                // print with prefix
+                rand.unparse(writer, leftPrec, rightPrec);
+            } else {
+                // print without prefix
+                writer.print("'");
+                if (rand.getTypeName() == SqlTypeName.Binary) {
+                    BitString bs = (BitString) rand.getValue();
+                    writer.print(bs.toHexString());
+                } else {
+                    writer.print(rand.toValue());
+                }
+                writer.print("'");
+            }
+        }
+        if (collation != null) {
+            writer.print(" ");
+            writer.print(collation.toString());
+        }
+    }
+
+    // test evaluated literals
+    public void test(SqlTester tester)
+    {
+        tester.checkString("'buttered'\n' toast'", "buttered toast");
+        tester.checkString("'corned'\n' beef'\n' on'\n' rye'",
+            "corned beef on rye");
+        tester.checkString("_latin1'Spaghetti'\n' all''Amatriciana'",
+            "Spaghetti all'Amatriciana");
+        tester.checkBoolean("B'0101'\n'0011' = B'01010011'",
+            Boolean.TRUE);
+        tester.checkBoolean("x'1234'\n'abcd' = x'1234abcd'",
+            Boolean.TRUE);
+    }
+}
---
+++ farrago/src/org/eigenbase/sql/fun/SqlSubstringFunction.java
@@ -0,0 +1,152 @@
+/*
+// $Id$
+// Package org.eigenbase is a class library of database components.
+// Copyright (C) 2002-2004 Disruptive Tech
+// Copyright (C) 2003-2004 John V. Sichi
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package org.eigenbase.sql.fun;
+
+import org.eigenbase.sql.*;
+import org.eigenbase.sql.test.SqlTester;
+import org.eigenbase.sql.type.SqlTypeName;
+import org.eigenbase.reltype.RelDataType;
+
+import java.util.ArrayList;
+
+/**
+ * Definition of the "SUBSTRING" builtin SQL function.
+ *
+ * @author Wael Chatila 
+ * @since Sep 5, 2004
+ * @version $Id$
+ */
+public class SqlSubstringFunction extends SqlFunction {
+
+    SqlSubstringFunction() {
+        super("SUBSTRING", SqlKind.Function,
+            SqlOperatorTable.useNullableVaryingFirstArgType, null, null,
+            SqlFunction.SqlFuncTypeName.String);
+    }
+
+    protected String getSignatureTemplate(final int operandsCount)
+    {
+        switch (operandsCount) {
+        case 2:
+            return "{0}({1} FROM {2})";
+        case 3:
+            return "{0}({1} FROM {2} FOR {3})";
+        }
+        assert (false);
+        return null;
+    }
+
+    public String getAllowedSignatures(String name)
+    {
+        StringBuffer ret = new StringBuffer();
+        for (int i = 0; i < SqlOperatorTable.stringTypes.length;
+             i++) {
+            if (i > 0) {
+                ret.append(NL);
+            }
+            ArrayList list = new ArrayList();
+            list.add(SqlOperatorTable.stringTypes[i]);
+            list.add(SqlTypeName.Integer);
+            ret.append(this.getAnonymousSignature(list));
+            ret.append(NL);
+            list.add(SqlTypeName.Integer);
+            ret.append(this.getAnonymousSignature(list));
+        }
+        return replaceAnonymous(
+            ret.toString(),
+            name);
+    }
+
+    protected void checkArgTypes(
+        SqlCall call,
+        SqlValidator validator,
+        SqlValidator.Scope scope)
+    {
+        int n = call.operands.length;
+        assert ((3 == n) || (2 == n));
+        SqlOperatorTable.typeNullableString.checkThrows(validator,
+            scope, call, call.operands[0], 0);
+        if (2 == n) {
+            SqlOperatorTable.typeNullableNumeric.checkThrows(validator,
+                scope, call, call.operands[1], 0);
+        } else {
+            RelDataType t1 =
+                validator.deriveType(scope, call.operands[1]);
+            RelDataType t2 =
+                validator.deriveType(scope, call.operands[2]);
+
+            if (t1.isCharType()) {
+                SqlOperatorTable.typeNullableString.checkThrows(validator,
+                    scope, call, call.operands[1], 0);
+                SqlOperatorTable.typeNullableString.checkThrows(validator,
+                    scope, call, call.operands[2], 0);
+
+                isCharTypeComparableThrows(validator, scope,
+                    call.operands);
+            } else {
+                SqlOperatorTable.typeNullableNumeric.checkThrows(validator,
+                    scope, call, call.operands[1], 0);
+                SqlOperatorTable.typeNullableNumeric.checkThrows(validator,
+                    scope, call, call.operands[2], 0);
+            }
+
+            if (!t1.isSameTypeFamily(t2)) {
+                throw call.newValidationSignatureError(validator, scope);
+            }
+        }
+    }
+
+    public SqlOperator.OperandsCountDescriptor getOperandsCountDescriptor()
+    {
+        return new SqlOperator.OperandsCountDescriptor(2, 3);
+    }
+
+    public void unparse(
+        SqlWriter writer,
+        SqlNode [] operands,
+        int leftPrec,
+        int rightPrec)
+    {
+        writer.print(name);
+        writer.print("(");
+        operands[0].unparse(writer, leftPrec, rightPrec);
+        writer.print(" FROM ");
+        operands[1].unparse(writer, leftPrec, rightPrec);
+
+        if (3 == operands.length) {
+            writer.print(" FOR ");
+            operands[2].unparse(writer, leftPrec, rightPrec);
+        }
+
+        writer.print(")");
+    }
+
+    public void test(SqlTester tester)
+    {
+        tester.checkString("substring('abc' from 1 for 2)", "ab");
+        tester.checkString("substring('abc' from 2)", "bc");
+
+        //substring reg exp not yet supported
+        //                    tester.checkString("substring('foobar' from '%#\"o_b#\"%' for '#')", "oob");
+        tester.checkNull("substring(cast(null as varchar),1,2)");
+    }
+}
