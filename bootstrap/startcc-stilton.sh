7fa9e1247bcf70e7c8df44e21749cbbc8e0b44d7
John V. Sichi <jsichi@gmail.com>
2004-09-19 23:15:35 -0800

    BOOTSTRAP:  fix stilton path
    [git-p4: depot-paths = "//open/dt/dev/": change = 2695]

--- bootstrap/startcc-stilton.sh
+++ bootstrap/startcc-stilton.sh
@@ -3,6 +3,6 @@
 export configSuffix=-win32
 ./initcc-open.sh
 
-export PATH=$PATH:/cruise/open/bootstrap/cruisecontrol-2.1.5/main/bin
+export PATH=$PATH:/cruise/bootstrap/cruisecontrol-2.1.5/main/bin
 export HOSTNAME
 nice cruisecontrol.sh -configfile config-stilton.xml -port 8080
