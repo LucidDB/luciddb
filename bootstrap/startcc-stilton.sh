5d186f8e91ffc98810ecbefcd8ca9d16e070cbb5
John V. Sichi <jsichi@gmail.com>
2004-09-19 23:21:20 -0800

    BOOTSTRAP:  use cruisecontrol.bat instead of cruisecontrol.sh
    [git-p4: depot-paths = "//open/dt/dev/": change = 2696]

--- bootstrap/startcc-stilton.sh
+++ bootstrap/startcc-stilton.sh
@@ -5,4 +5,5 @@ export configSuffix=-win32
 
 export PATH=$PATH:/cruise/bootstrap/cruisecontrol-2.1.5/main/bin
 export HOSTNAME
-nice cruisecontrol.sh -configfile config-stilton.xml -port 8080
+export JAVA_HOME=d:\\j2sdk1.4.2_03
+cmd /c cruisecontrol.bat -configfile config-stilton.xml -port 8080
