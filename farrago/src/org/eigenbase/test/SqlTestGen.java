d9fc032f1b9a848d18e67f9dcecd774ff9fb6511
Julian Hyde <julianhyde(@)ignore.gmail.com>
2004-11-10 15:42:12 -0800

    FARRAGO: Generate SQL script from SqlValidatorTestCase (not enabled as a test just yet).
    [git-p4: depot-paths = "//open/dt/dev/": change = 2838]

---
+++ farrago/src/org/eigenbase/test/SqlTestGen.java
@@ -0,0 +1,174 @@
+/*
+// $Id$
+// Package org.eigenbase is a class library of database components.
+// Copyright (C) 2004-2004 Disruptive Tech
+// Copyright (C) 2003-2004 John V. Sichi
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+package org.eigenbase.test;
+
+import org.eigenbase.sql.SqlValidator;
+import org.eigenbase.sql.SqlCollation;
+import org.eigenbase.sql.parser.SqlParser;
+import org.eigenbase.sql.parser.ParseException;
+import org.eigenbase.util.Util;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+/**
+ * Utility to generate SQL a script from validator test.
+ *
+ * @author jhyde
+ * @since Nov 10, 2004
+ * @version $Id$
+ **/
+public class SqlTestGen {
+    public static void main(String[] args) {
+        new SqlTestGen().genValidatorTest();
+    }
+
+    private void genValidatorTest() {
+        FileOutputStream fos = null;
+        PrintWriter pw = null;
+        try {
+            File file = new File("validatorTest.sql");
+            fos = new FileOutputStream(file);
+            pw = new PrintWriter(fos);
+            Method[] methods = getJunitMethods(SqlValidatorSpooler.class);
+            for (int i = 0; i < methods.length; i++) {
+                Method method = methods[i];
+                final SqlValidatorSpooler test =
+                    new SqlValidatorSpooler(method.getName(), pw);
+                final Object result = method.invoke(test, new Object[0]);
+                assert result == null;
+            }
+        } catch (IOException e) {
+            throw Util.newInternal(e);
+        } catch (IllegalAccessException e) {
+            throw Util.newInternal(e);
+        } catch (IllegalArgumentException e) {
+            throw Util.newInternal(e);
+        } catch (InvocationTargetException e) {
+            throw Util.newInternal(e);
+        } finally {
+            if (pw != null) {
+                pw.flush();
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    throw Util.newInternal(e);
+               }
+            }
+        }
+    }
+
+    /**
+     * Returns a list of all of the Junit methods in a given class.
+     */
+    private static Method[] getJunitMethods(Class clazz) {
+        final Method[] methods = clazz.getMethods();
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < methods.length; i++) {
+            Method method = methods[i];
+            if (method.getName().startsWith("test") &&
+                Modifier.isPublic(method.getModifiers()) &&
+                !Modifier.isStatic(method.getModifiers()) &&
+                method.getParameterTypes().length == 0 &&
+                method.getReturnType() == Void.TYPE) {
+                list.add(method);
+            }
+        }
+        return (Method[]) list.toArray(new Method[list.size()]);
+    }
+
+    /**
+     * Subversive subclass, which spools restuls to a writer rather than
+     * running tests. It is not a valid JUnit test because it does not have
+     * a public constructor.
+     */
+    private static class SqlValidatorSpooler extends SqlValidatorTestCase {
+        private final String testName;
+        private final PrintWriter pw;
+
+        private SqlValidatorSpooler(String testName, PrintWriter pw) {
+            this.testName = testName;
+            this.pw = pw;
+        }
+
+        public SqlValidator getValidator() {
+            throw new UnsupportedOperationException();
+        }
+
+        public SqlParser getParser(String sql) throws ParseException {
+            throw new UnsupportedOperationException();
+        }
+
+        public void checkType(
+            String sql,
+            String expected)
+        {
+            // We could generate the SQL -- or maybe describe but ignore it for
+            // now.
+        }
+
+        public void checkCharset(
+            String sql,
+            Charset expectedCharset)
+        {
+            // We could generate the SQL -- or maybe describe but ignore it for
+            // now.
+        }
+
+        public void checkCollation(
+            String sql,
+            String expectedCollationName,
+            SqlCollation.Coercibility expectedCoercibility)
+        {
+            // We could generate the SQL -- or maybe describe but ignore it for
+            // now.
+        }
+
+        protected void assertExceptionIsThrown(
+            String sql,
+            String expectedMsgPattern,
+            int expectedLine,
+            int expectedColumn)
+        {
+            if (expectedMsgPattern == null) {
+                // This SQL statement is supposed to succeed. Generate it to
+                // the file, so we can see what output it produces.
+                pw.println("-- " + testName);
+                pw.println(sql);
+                pw.println(";");
+            } else {
+                // Do nothing. We know that this fails the validator test,
+                // so we don't learn anything by having it fail from SQL.
+            }
+        }
+    }
+}
+
+// End SqlTestGen.java
