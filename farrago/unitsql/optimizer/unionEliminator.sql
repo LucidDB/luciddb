0dc8edc53b4ea94c357f7b3eb0418f75777c036d
wael <a@b>
2005-02-04 22:23:45 -0800

    FARRAGO: add test case for UnionEliminatorRule
    [git-p4: depot-paths = "//open/dt/dev/": change = 3154]

---
+++ farrago/unitsql/optimizer/unionEliminator.sql
@@ -0,0 +1,6 @@
+-- $Id$
+-- Test optimization rule to elimnate the use of UNION 
+-- when a UNION call only has one input
+
+-- should not contain any calls to any union xo
+explain plan for select multiset[622] from values(true);
