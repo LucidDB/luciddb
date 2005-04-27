e1659c0bcd681a17b3fa20d99a5898e64bde303f
John V. Sichi <jsichi@gmail.com>
2005-04-27 15:00:10 -0800

    FARRAGO:  fix trailing space in Win32 trace config file
    [git-p4: depot-paths = "//open/dev/": change = 3503]

--- farrago/dist/install/install.bat
+++ farrago/dist/install/install.bat
@@ -12,12 +12,12 @@ set TRACE_CONFIG=%TRACE_DIR%\Trace.properties
 if "%JAVA_HOME%" == "" goto need_java
 
 if not exist %TRACE_DIR% mkdir %TRACE_DIR%
-echo # Tracing configuration > %TRACE_CONFIG%
-echo handlers=java.util.logging.FileHandler >> %TRACE_CONFIG%
-echo java.util.logging.FileHandler.append=true >> %TRACE_CONFIG%
-echo java.util.logging.FileHandler.formatter=java.util.logging.SimpleFormatter >> %TRACE_CONFIG%
-echo java.util.logging.FileHandler.pattern=%TRACE_DIR:\=\\%\\Trace.log >> %TRACE_CONFIG%
-echo .level=CONFIG >> %TRACE_CONFIG%
+echo # Tracing configuration> %TRACE_CONFIG%
+echo handlers=java.util.logging.FileHandler>> %TRACE_CONFIG%
+echo java.util.logging.FileHandler.append=true>> %TRACE_CONFIG%
+echo java.util.logging.FileHandler.formatter=java.util.logging.SimpleFormatter>> %TRACE_CONFIG%
+echo java.util.logging.FileHandler.pattern=%TRACE_DIR:\=\\%\\Trace.log>> %TRACE_CONFIG%
+echo .level=CONFIG>> %TRACE_CONFIG%
 
 echo set JAVA_HOME=%JAVA_HOME%> %BIN_DIR%\classpath.bat
 echo set LCP=%JAVA_HOME%\lib\tools.jar>> %BIN_DIR%\classpath.bat
