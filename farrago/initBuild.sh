b81d63420f19df7b2d7db6716fdd17d7d4bfda0c
Stephan Zuercher <szuercher@lucidera.com>
2005-06-06 10:03:29 -0800

    FARRAGO: add --with[out]-tests to initBuild.sh (defaults to without),                 modify command line handling to take args in any order FENNEL: add --with[out]-tests to initBuild.sh (defaults to without),                modify command line handling to take args in any order        BOOTSTRAP: call initBuild.sh scripts "--with-tests"
    [git-p4: depot-paths = "//open/dt/dev/": change = 3658]

--- farrago/initBuild.sh
+++ farrago/initBuild.sh
@@ -24,48 +24,47 @@
 # an existing one after syncing changes from source control.
 
 usage() {
-    echo "Usage:  initBuild.sh --with[out]-fennel [--append-init-properties] [--with[out]-optimization] [--with[out]-debug] [--skip-thirdparty]"
+    echo "Usage:  initBuild.sh --with[out]-fennel [--append-init-properties] [--with[out]-optimization] [--with[out]-debug] [--skip-thirdparty] [--with[out]-tests]"
 }
 
-if [ "$1" == "--with-fennel" ] ; then
-    fennel_disabled=false
+fennel_flag_missing=true
+fennel_disabled=
+append_init_build_props=false
+skip_tests=true
+
+# extended globbing for case statement
+shopt -sq extglob
+
+while [ -n "$1" ]; do
+    case $1 in
+        --with-fennel) fennel_disabled=false; fennel_flag_missing=false;;
+        --without-fennel) fennel_disabled=true; fennel_flag_missing=false;;
+        --append-init-properties) append_init_build_props=true;;
+        --with?(out)-optimization) OPT_FLAG="$1";;
+        --with?(out)-debug) DEBUG_FLAG="$1";;
+        --skip-thirdparty) THIRDPARTY_FLAG="$1";;
+        --with-tests) skip_tests=false; TEST_FLAG="$1";;
+        --without-tests) skip_tests=true; TEST_FLAG="$1";;
+
+        *) usage; exit -1;;
+    esac
     shift
-elif [ "$1" == "--without-fennel" ] ; then
-    fennel_disabled=true
-    shift
-else
+done
+
+shopt -uq extglob
+
+# Check required options
+if $fennel_flag_missing; then
     usage
-    exit -1
+    exit -1;
 fi
 
-if [ "$1" == "--append-init-properties" ] ; then
+if $append_init_build_props; then
     touch initBuild.properties
     shift
 else
     # default is remove this file
     rm -f initBuild.properties
-fi
-
-if [ "$1" == "--with-optimization" -o "$1" == "--without-optimization" ] ; then
-    OPT_FLAG="$1"
-    shift
-fi
-
-if [ "$1" == "--with-debug" -o "$1" == "--without-debug" ] ; then
-    DEBUG_FLAG="$1"
-    shift
-fi
-
-if [ "$1" == "--skip-thirdparty" ] ; then
-    THIRDPARTY_FLAG="$1"
-    shift
-else
-    THIRDPARTY_FLAG=""
-fi
-
-if [ -n "$1" ] ; then
-    usage
-    exit -1;
 fi
 
 # Set up Farrago custom build properties file
@@ -93,7 +92,7 @@ if $fennel_disabled ; then
     echo Skipping Fennel build
 else
     cd ../fennel
-    ./initBuild.sh --with-farrago $OPT_FLAG $DEBUG_FLAG $THIRDPARTY_FLAG
+    ./initBuild.sh --with-farrago $OPT_FLAG $DEBUG_FLAG $THIRDPARTY_FLAG $TEST_FLAG
 
     # Set up Fennel runtime environment
     . fennelenv.sh `pwd`
@@ -104,7 +103,7 @@ fi
 cd ../farrago
 ant clean
 
-if $fennel_disabled ; then
+if $fennel_disabled || $skip_tests ; then
     ant createCatalog
 else
     ant test
