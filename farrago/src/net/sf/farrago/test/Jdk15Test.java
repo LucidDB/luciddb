1258bc8fd094da5e6711d23fe18ffe305d15bf5e
John V. Sichi <jsichi@gmail.com>
2005-07-05 22:32:40 -0800

    FARRAGO:  add experimental retroweave target (disabled)
    [git-p4: depot-paths = "//open/dt/dev/": change = 3780]

---
+++ farrago/src/net/sf/farrago/test/Jdk15Test.java
@@ -0,0 +1,75 @@
+/*
+// $Id$
+// Farrago is an extensible data management system.
+// Copyright (C) 2005-2005 The Eigenbase Project
+// Copyright (C) 2005-2005 Disruptive Tech
+// Copyright (C) 2005-2005 LucidEra, Inc.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
+// any later version approved by The Eigenbase Project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+package net.sf.farrago.test;
+
+import junit.framework.*;
+
+import java.util.*;
+
+/**
+ * Jdk15Test tests language features introduced in JDK 1.5.  It is excluded
+ * from compilation when src="1.4" is passed to javac.
+ *
+ * @author John V. Sichi
+ * @version $Id$
+ */
+public class Jdk15Test extends TestCase
+{
+    public Jdk15Test(String testName)
+    {
+        super(testName);
+    }
+
+    public void testGenericCollections()
+    {
+        List<String> words = new ArrayList<String>();
+        words.add("left");
+        words.add("right");
+        words.add("drat");
+        
+        // remove four-letter words
+        Iterator<String> iter = words.iterator();
+        while (iter.hasNext()) {
+            String s = iter.next();
+            if (s.length() == 4) {
+                iter.remove();
+            }
+        }
+
+        assertEquals(1, words.size());
+        assertEquals("right", words.get(0));
+    }
+
+    public void testForeach()
+    {
+        List<String> words = new ArrayList<String>();
+        words.add("oingo");
+        words.add("boingo");
+        words.add("ringo");
+
+        for (String s : words) {
+            assertTrue(s.endsWith("ingo"));
+        }
+    }
+}
+
+// End Jdk15Test.java
