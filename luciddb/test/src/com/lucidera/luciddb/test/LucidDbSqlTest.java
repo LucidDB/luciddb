d046f264fe96c6939acefea2b12e6ac09773e77d
Sunny Choi <skchoi56@yahoo.com>
2005-11-16 16:28:21 -0800

    LUCIDDB: format files; no functional changes
    [git-p4: depot-paths = "//open/lu/dev/": change = 4408]

--- luciddb/test/src/com/lucidera/luciddb/test/LucidDbSqlTest.java
+++ luciddb/test/src/com/lucidera/luciddb/test/LucidDbSqlTest.java
@@ -1,3 +1,23 @@
+/*
+// $Id$
+// LucidDB is a DBMS optimized for business intelligence.
+// Copyright (C) 2005-2005 LucidEra, Inc.
+// Copyright (C) 2005-2005 The Eigenbase Project
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
+// any later version approved by The Eigenbase Project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 package com.lucidera.luciddb.test;
 
 import java.io.*;
@@ -6,8 +26,11 @@ import net.sf.farrago.util.*;
 
 
 /**
+ * LucidDbSqlTest refines {@link net.sf.farrago.test.FarragoSqlTest} 
+ * with LucidDB-specific test infrastructure.
  *
- * See: //open/lu/dev/farrago/src/net/sf/farrago/test/FarragoSqlTest.java 
+ * @author Sunny Choi
+ * @version $Id$
  */
 public class LucidDbSqlTest extends LucidDbTestCase
 {
@@ -18,24 +41,24 @@ public class LucidDbSqlTest extends LucidDbTestCase
         super(testName);
     }
     
-    
+
     public static Test suite()
         throws Exception
     {
         return gatherSuite(
-                           FarragoProperties.instance().testFilesetUnitsql.get(true),
-                           new LucidDbSqlTestFactory() {
-                               public LucidDbTestCase createSqlTest(String testName)
-                                   throws Exception
-                               {
-                                   return new LucidDbSqlTest(testName);
-                               }
-                           });
+            FarragoProperties.instance().testFilesetUnitsql.get(true),
+            new LucidDbSqlTestFactory() {
+                public LucidDbTestCase createSqlTest(String testName)
+                    throws Exception
+                {
+                    return new LucidDbSqlTest(testName);
+                }
+            });
     }
 
     protected static Test gatherSuite(
-                                      String fileSet,
-                                      LucidDbSqlTestFactory fac)
+        String fileSet,
+        LucidDbSqlTestFactory fac)
         throws Exception
     {
         StringReader stringReader = new StringReader(fileSet);
