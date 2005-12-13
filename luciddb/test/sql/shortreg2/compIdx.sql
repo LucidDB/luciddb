34d704752b72ede4a9e13ef39a53a7aa58e69fe7
Sunny Choi <skchoi56@yahoo.com>
2005-12-12 17:10:39 -0800

    LUCIDDB: enable tests using either csvjdbc or flatfile driver; temporary location for now.
    [git-p4: depot-paths = "//open/lu/dev/": change = 4673]

---
+++ luciddb/test/sql/shortreg2/compIdx.sql
@@ -0,0 +1,24 @@
+--
+-- composite index tests
+--
+
+set schema 's';
+
+-- leading edge searches
+
+select LNAME from customers where LNAME='Dole' order by 1;
+select LNAME from customers where LNAME='Peterson' order by 1;
+select LNAME from customers where LNAME='Andrews' order by 1;
+
+select LNAME from customers where LNAME<='Dole' order by 1;
+select LNAME,FNAME from customers where LNAME>='Peterson' order by 1,2;
+select LNAME,CUSTID from customers where LNAME<='Andrews' order by 1,2;
+
+select LNAME from customers where LNAME<'Dole' order by 1;
+select LNAME,FNAME from customers where LNAME>'Peterson' order by 1,2;
+select LNAME,CUSTID from customers where LNAME<'Andrews' order by 1,2;
+
+-- full searches
+
+select * from customers where LNAME='Bush' and FNAME='Gerry' order by 1;
+select LNAME from customers where LNAME='Frank' and FNAME='Victor' order by 1;
