91ed19bea073d7795f81148a92f37af835ac1978
Sunny Choi <skchoi56@yahoo.com>
2005-12-22 16:29:13 -0800

    LUCIDDB: checking bench load results
    [git-p4: depot-paths = "//open/lu/dev/": change = 4803]

--- luciddb/test/sql/bench/benchqueries.sql
+++ luciddb/test/sql/bench/benchqueries.sql
@@ -1,5 +1,6 @@
 set schema 's';
 
+-- overflows. change from INTEGER to NUMERIC later.
 select sum("k500k") from bench100K group by "k2" order by 1;
 select sum("k500k") from bench100K group by "k5" order by 1;
 select sum("k500k") from bench100K group by "k10" order by 1;
@@ -10,3 +11,17 @@ select sum("k10k") from bench100K group by "k5" order by 1;
 select sum("k10k") from bench100K group by "k10" order by 1;
 select sum("k10k") from bench100K group by "k25" order by 1;
 
+select max("k10k") from bench100K group by "k2" order by 1;
+select max("k10k") from bench100K group by "k5" order by 1;
+select max("k10k") from bench100K group by "k10" order by 1;
+select max("k10k") from bench100K group by "k25" order by 1;
+
+select min("k10k") from bench100K group by "k2" order by 1;
+select min("k10k") from bench100K group by "k5" order by 1;
+select min("k10k") from bench100K group by "k10" order by 1;
+select min("k10k") from bench100K group by "k25" order by 1;
+
+select count("k10k") from bench100K group by "k2" order by 1;
+select count("k10k") from bench100K group by "k5" order by 1;
+select count("k10k") from bench100K group by "k10" order by 1;
+select count("k10k") from bench100K group by "k25" order by 1;
