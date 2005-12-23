e81f9d3e85376f6549906b3ef911d50de187eaaf
Sunny Choi <skchoi56@yahoo.com>
2005-12-23 14:24:47 -0800

    LUCIDDB: copy all remaining shortreg sql to shortreg2. shortreg2 should later replace shortreg completely.
    [git-p4: depot-paths = "//open/lu/dev/": change = 4818]

---
+++ luciddb/test/sql/shortreg2/analyze.sql
@@ -0,0 +1,6 @@
+analyze table EMP estimate statistics for all columns SAMPLE 100 PERCENT;
+analyze table PRODUCTS estimate statistics for all columns SAMPLE 100 PERCENT;
+analyze table SALES estimate statistics for all columns SAMPLE 100 PERCENT;
+analyze table CUSTOMERS estimate statistics for all columns SAMPLE 100 PERCENT;
+analyze table LOCATION estimate statistics for all columns  SAMPLE 100 PERCENT;
+analyze table DEPT estimate statistics for all columns   SAMPLE 100 PERCENT;
