5ed84b0e9aad501547403ef12444f2e380373a1f
Sunny Choi <skchoi56@yahoo.com>
2005-12-23 16:02:41 -0800

    LUCIDDB: use double, not integer for bench table columns
    [git-p4: depot-paths = "//open/lu/dev/": change = 4820]

--- luciddb/test/sql/bench/benchqueries.sql
+++ luciddb/test/sql/bench/benchqueries.sql
@@ -1,6 +1,5 @@
 set schema 's';
 
--- overflows. change from INTEGER to NUMERIC later.
 select sum("k500k") from bench100K group by "k2" order by 1;
 select sum("k500k") from bench100K group by "k5" order by 1;
 select sum("k500k") from bench100K group by "k10" order by 1;
