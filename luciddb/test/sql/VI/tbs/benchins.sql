b56ad819f5f8ea4317f39117fa95c455da64d867
Elizabeth Lin <elin@lucidera.com>
2006-03-22 19:47:10 -0800

    LUCIDDB/QA: adding missed/wrong test file for VI/tbs select tests
    [git-p4: depot-paths = "//open/lu/dev/": change = 5948]

--- luciddb/test/sql/VI/tbs/benchins.sql
+++ luciddb/test/sql/VI/tbs/benchins.sql
@@ -1,19 +1,3 @@
-create foreign data wrapper test_jdbc
-library '${FARRAGO_HOME}/plugin/FarragoMedJdbc3p.jar'
-language java;
-
-create server orcl_server
-foreign data wrapper test_jdbc
-options(
-    url 'jdbc:oracle:thin:@akela.lucidera.com:1521:XE',
-    user_name 'schoi',
-    password 'schoi',
-    driver_class 'oracle.jdbc.driver.OracleDriver'
-);
-
-create schema orcl_schema;
-create schema s;
-
 set schema 's';
 
 CREATE TABLE BENCH100 (
