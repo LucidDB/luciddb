c308f581aec65727f40658f4b75710bf787918b6
Marc Berkowitz <marc.berkowitz(@)sqlstream.ignore.com>
2006-04-02 14:37:50 -0800

    FARRAGO: extend unit test of "not null" constraint;  Test inserting multiple values, some null.  Test both java and fennel calculators.  Note that they behave differently for multiple values:   - java rejects the whole insert statement   - fennel rejects only the invalid rows (cf @6082).
    [git-p4: depot-paths = "//open/dt/dev/": change = 6083]

--- farrago/unitsql/constraints/notnull.sql
+++ farrago/unitsql/constraints/notnull.sql
@@ -9,3 +9,20 @@ insert into sales.depts values (null,'Nullification');
 
 -- NOT NULL enforcement:  should fail due to implicit NULL
 insert into sales.emps(name,empno) values ('wael',300);
+
+-- test inserting multiple values, some wih nulls. Only the nulls should be rejected
+-- REVIEW: the java calc rejects all, the fennel calc rejects only the nulls.
+-- Which is correct?
+create table sales.fake(
+    deptno integer not null primary key, 
+    name varchar(128) not null);
+insert into sales.fake values (null, 'Null'),(100, 'Fish'), (null, 'Null Again'), (200, 'Fowl'), (null, 'and again');
+select * from sales.fake;
+
+-- repeat with Fennel calc
+alter system set "calcVirtualMachine"='CALCVM_FENNEL';
+insert into sales.fake values (null, 'Null');
+insert into sales.fake values (null, 'Null Again'),(100, 'Fish'), (null, 'again'), (200, 'Fowl'), (null, 'and again');
+select * from sales.fake;
+
+drop table sales.fake;
