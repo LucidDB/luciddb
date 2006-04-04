4e9e8e5f3d389873242e48b6b750d7f015afa4c6
Elizabeth Lin <elin@lucidera.com>
2006-04-03 23:08:33 -0800

    QA/LUCIDDB: sql files for join tests
    [git-p4: depot-paths = "//open/lu/dev/": change = 6097]

--- luciddb/test/sql/company/joinMisc.sql
+++ luciddb/test/sql/company/joinMisc.sql
@@ -10,10 +10,10 @@ from EMP, DEPT
 where EMP.DEPTNO + 10 = DEPT.DEPTNO - 10
 order by EMP.EMPNO;
 
-select EMP.LNAME, DEPT.DNAME
-from EMP, DEPT
-where substring(EMP.LNAME,2,1)=substring(DEPT.DNAME,2,1)
-order by EMP.EMPNO, DEPT.DNAME;
+--select EMP.LNAME, DEPT.DNAME
+--from EMP, DEPT
+--where substring(EMP.LNAME,2,1)=substring(DEPT.DNAME,2,1)
+--order by EMP.EMPNO, DEPT.DNAME;
 
 -- one side functions only
 
@@ -30,22 +30,22 @@ select DEPT.DNAME, LOCATION.CITY from DEPT, LOCATION
 order by DEPT.DEPTNO, LOCATION.LOCID;
 
 -- Equjoin Condition plus non-equjoin condition on the same tables with group by ( Bugid 2796 )
-select P.DEPTNO, COUNT(*) from 
-(select DEPTNO, AVG(EMPNO) AVGAMT from
-EMP group by DEPTNO) A,
-EMP P
-where P.DEPTNO = A.DEPTNO
-and EMPNO < AVGAMT
-group by P.DEPTNO 
-order by P.DEPTNO;
+--select P.DEPTNO, COUNT(*) from 
+--(select DEPTNO, AVG(EMPNO) AVGAMT from
+--EMP group by DEPTNO) A,
+--EMP P
+--where P.DEPTNO = A.DEPTNO
+--and EMPNO < AVGAMT
+--group by P.DEPTNO 
+--order by P.DEPTNO;
    
 -- Equjoin Condition plus non-equjoin condition on the same tables with group by and non-staged Item in subquery
-select P.DEPTNO, COUNT(*) from
-(select DEPTNO, INC() , AVG(EMPNO) AVGAMT from
-EMP group by DEPTNO) A,
-EMP P
-where P.DEPTNO = A.DEPTNO
-and EMPNO < AVGAMT
-group by P.DEPTNO
-order by P.DEPTNO;
+--select P.DEPTNO, COUNT(*) from
+--(select DEPTNO, INC() , AVG(EMPNO) AVGAMT from
+--EMP group by DEPTNO) A,
+--EMP P
+--where P.DEPTNO = A.DEPTNO
+--and EMPNO < AVGAMT
+--group by P.DEPTNO
+--order by P.DEPTNO;
 
