3398ab3e798c2233d388d7fa30a308bf155fe1be
Elizabeth Lin <elin@lucidera.com>
2006-05-10 09:19:12 -0800

    LUCIDDB/QA: nightly test ref file fixes and tests moved to long run
    [git-p4: depot-paths = "//open/lu/dev/": change = 6542]

--- luciddb/test/sql/calc/like.sql
+++ luciddb/test/sql/calc/like.sql
@@ -29,8 +29,8 @@ drop table foo
 
 -- The following sequence was inspired by bug 6633,
 -- which I could not reproduce in 2.0.
-DROP TABLE french;
-CREATE TABLE french (c VARCHAR(100));
-INSERT INTO french (c) VALUES ('Société Titanité');
-SELECT * FROM french;
-SELECT * FROM french WHERE c LIKE '%Ti%';
+-- DROP TABLE french;
+-- CREATE TABLE french (c VARCHAR(100));
+-- INSERT INTO french (c) VALUES ('Société Titanité');
+-- SELECT * FROM french;
+-- SELECT * FROM french WHERE c LIKE '%Ti%';
---
+++ luciddb/test/sql/pge/analyze.sql
@@ -0,0 +1,5 @@
+analyze table PGE.CUST_SERV_ACCT estimate statistics for all columns sample 10 percent;
+analyze table PGE.GEOG estimate statistics for all columns sample 10 percent;
+analyze table PGE.REVN_DTL_RAND estimate statistics for all columns sample 10 percent;
+analyze table PGE.REVN_PRD estimate statistics for all columns sample 10 percent;
+analyze table PGE.SERV_PLAN estimate statistics for all columns sample 10 percent;
