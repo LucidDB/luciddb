f07eb394f58a49277a03c379b54581cd75157c96
Fan Zhang <fzhang@lucidera.com>
2006-05-16 21:44:45 -0800

    BUILD: build release tar file by default so platform build does not have to build each time
    [git-p4: depot-paths = "//open/lu/dev/": change = 6598]

--- luciddb/initBuild.sh
+++ luciddb/initBuild.sh
@@ -88,6 +88,10 @@ else
     ${run_ant} test
 fi
 
+cd ${luciddb_dir}/../farrago
+cp -f ./dist/ExampleRelease.properties ./dist/FarragoRelease.properties
+./distBuild.sh --without-init-build
+
 nightlylog_dir=${luciddb_dir}/nightlylog
 nightly_test_list="\
 test-nondb \
