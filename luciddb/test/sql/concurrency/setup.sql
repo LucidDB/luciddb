e50aa104f48adc7927bff83d7a3d504a87ea5d07
Khanh Vu <kvu@lucidera.com>
2006-08-17 09:07:33 -0800

    LUCIDDB-TEST: concurrency tests for local and foreign db(flatfile, oracle, mssqlserver, csvjdbc)
    [git-p4: depot-paths = "//open/lu/dev/": change = 7413]

--- luciddb/test/sql/concurrency/setup.sql
+++ luciddb/test/sql/concurrency/setup.sql
@@ -4,3 +4,11 @@ create schema concurrency;
 
 create table concurrency.test(i int);
 insert into concurrency.test values(42);
+
+create table concurrency.t1(c integer);
+create table concurrency.t2(c integer);
+
+
+create schema concurrency2;
+create table concurrency2.t1(c integer);
+create table concurrency2.t2(c integer);
