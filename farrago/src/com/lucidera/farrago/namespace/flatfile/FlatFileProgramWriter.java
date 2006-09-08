84584adbaff7d4593485f2b41338a29524bca371
John Pham <jvp256@yahoo.com>
2006-09-08 07:24:24 -0800

    LU/FARRAGO: flat file reader custom date formats
    [git-p4: depot-paths = "//open/lu/dev/": change = 7595]

--- farrago/src/com/lucidera/farrago/namespace/flatfile/FlatFileProgramWriter.java
+++ farrago/src/com/lucidera/farrago/namespace/flatfile/FlatFileProgramWriter.java
@@ -22,16 +22,31 @@ package com.lucidera.farrago.namespace.flatfile;
 
 import com.disruptivetech.farrago.calc.*;
 
+import net.sf.farrago.query.*;
+
 import org.eigenbase.rel.*;
 import org.eigenbase.reltype.*;
 import org.eigenbase.rex.*;
+import org.eigenbase.sql.*;
+import org.eigenbase.sql.parser.*;
 import org.eigenbase.sql.type.*;
+import org.eigenbase.util.*;
 
 
 /**
- * Constructs a Calculator program for translating text from a flat file into
- * typed data. It is assumed that the text has already been processed for
- * quoting and escape characters.
+ * FlatFileProgramWriter builds calculator programs for converting 
+ * text from flat files into typed data. It assumes the text has already
+ * been processed for quoting and escape characters. The programs mainly 
+ * consist of simple casts. However, custom date formats may be specified 
+ * when using LucidDb. They are implemented with calls to LucidDb user 
+ * defined routines. Only one set of formats can be active for a writer.
+ * 
+ * <p>
+ * 
+ * When using custom date conversions, the program can be split into 
+ * Fennel and Java-only sections. If splitting the program, data must 
+ * flow through the Fennel section first, which handles basic casts. 
+ * Then the Java-only section handles custom date conversions.
  *
  * @author jpham
  * @version $Id$
@@ -43,77 +58,200 @@ public class FlatFileProgramWriter
 
     public static final int FLAT_FILE_MAX_NON_CHAR_VALUE_LEN = 255;
 
+    private static final SqlIdentifier toDateFuncName = 
+        new SqlIdentifier(
+            new String[] {"LOCALDB","APPLIB","CHAR_TO_DATE"},
+            new SqlParserPos(0,0));
+    private static final SqlIdentifier toTimeFuncName = 
+        new SqlIdentifier(
+            new String[] {"LOCALDB","APPLIB","CHAR_TO_TIME"},
+            new SqlParserPos(0,0));
+    private static final SqlIdentifier toTimestampFuncName = 
+        new SqlIdentifier(
+            new String[] {"LOCALDB","APPLIB","CHAR_TO_TIMESTAMP"},
+            new SqlParserPos(0,0));
+
+    private static final int DATETIME_FORMAT_ARG_LENGTH = 50;
+
     //~ Instance fields --------------------------------------------------------
 
-    RexBuilder rexBuilder;
-    RelDataTypeFactory typeFactory;
-    RelNode rel;
+    private final RexBuilder rexBuilder;
+    private final RelDataTypeFactory typeFactory;
+    private final FarragoPreparingStmt stmt;
+    private final String dateFormat;
+    private final String timeFormat;
+    private final String timestampFormat;
+    private final RelDataType[] datetimeArgs;
+    private final RelDataType rowType;
 
     //~ Constructors -----------------------------------------------------------
 
+    /**
+     * Constructs a new FlatFileProgramWriter
+     * 
+     * @param rexBuilder a rex node builder
+     * @param stmt the statement being prepared
+     * @param params a set of flat file server parameters. These parameters 
+     *   may include custom date formats
+     * @param rowType the desired data type for data read from a flat file. 
+     *   The row type is also used to infer the size of text fields in the 
+     *   file.
+     */
     public FlatFileProgramWriter(
-        RelNode rel)
+        RexBuilder rexBuilder,
+        FarragoPreparingStmt stmt,
+        FlatFileParams params,
+        RelDataType rowType)
     {
-        this.rel = rel;
-        rexBuilder = rel.getCluster().getRexBuilder();
+        this.rexBuilder = rexBuilder;
         typeFactory = rexBuilder.getTypeFactory();
+        this.stmt = stmt;
+        dateFormat = params.getDateFormat();
+        timeFormat = params.getTimeFormat();
+        timestampFormat = params.getTimestampFormat();
+        datetimeArgs = new RelDataType[2];
+        this.rowType = rowType;
     }
 
     //~ Methods ----------------------------------------------------------------
 
     /**
-     * Given the description of the expected data types, generates a program for
-     * converting text into typed data.
-     *
-     * <p>First this method infers the description of text columns required to
-     * read the exptected data values. Then it constructs the casts necessary to
-     * perform data conversion. Date conversions may require special functions.
+     * Gets the entire data conversion program
      */
-    public RexProgram getProgram(RelDataType rowType)
+    public RexProgram getProgram()
+    {
+        return getSection(Section.ALL);
+    }
+
+    /**
+     * Gets the Fennel portion of the data conversion program
+     */
+    public RexProgram getFennelSection()
+    {
+        return getSection(Section.FENNEL);
+    }
+
+    /**
+     * Gets the Java only portion of the data conversion program
+     * @return the program, or null if it is empty
+     */
+    public RexProgram getJavaOnlySection()
+    {
+        return getSection(Section.JAVA_ONLY);
+    }
+
+    /**
+     * Identifies a section of a flat file program
+     */
+    private enum Section {
+        /** 
+         * Fennel compatible program section, may include the entire program, 
+         * but does not include custom datetime conversions.
+         */
+        FENNEL,
+        /**
+         * Fennel incompatible program section. Includes custom datetime 
+         * conversions, but not ISO datetime conversions. May be empty.
+         */
+        JAVA_ONLY,
+        /**
+         * Includes the entire program 
+         */
+        ALL
+    }
+
+    /**
+     * Builds a section of the data conversion program as follows.
+     * 
+     * <ul>
+     *   <li>FENNEL: all inputs are text columns. Most inputs are casted to 
+     *     target types, except custom date fields, which are projected
+     *   <li>JAVA_ONLY: most inputs are typed data, except custom date fields, 
+     *     which are text columns. The typed fields are projected, while the 
+     *     date fields are converted with user defined routines.
+     *   <li>ALL: all inputs are text columns and all are converted to target 
+     *     types, either by casting or by UDRs.
+     * </ul>
+     * 
+     * @param section section of the program to build
+     * @return the program section
+     */
+    private RexProgram getSection(
+        Section section)
     {
         assert (rowType.isStruct());
         RelDataTypeField [] targetTypes = rowType.getFields();
+
+        // check for empty section
+        if (section == Section.JAVA_ONLY) {
+            boolean hasCustom = false;
+            for (int i = 0; i < targetTypes.length; i++) {
+                if (isCustom(targetTypes[i].getType())) {
+                    hasCustom = true;
+                    break;
+                }
+            }
+            if (! hasCustom) {
+                return null;
+            }
+        }
 
         // infer source text types
         RelDataType [] sourceTypes = new RelDataType[targetTypes.length];
         String [] sourceNames = new String[targetTypes.length];
         for (int i = 0; i < targetTypes.length; i++) {
             RelDataType targetType = targetTypes[i].getType();
-            sourceTypes[i] = getTextType(targetType);
+            if (section == Section.JAVA_ONLY
+                && (!isCustom(targetType))) 
+            {
+                sourceTypes[i] = targetType;
+            } else {
+                sourceTypes[i] = getTextType(targetType);
+            }
             sourceNames[i] = targetTypes[i].getName();
         }
         RelDataType inputRowType =
             typeFactory.createStructType(sourceTypes, sourceNames);
-
+        
         // construct rex program
         RexProgramBuilder programBuilder =
             new RexProgramBuilder(inputRowType, rexBuilder);
         for (int i = 0; i < targetTypes.length; i++) {
             RelDataType targetType = targetTypes[i].getType();
+            boolean isCustom = isCustom(targetType);
 
-            // TODO: call dedicated functions for conversion of dates
-            programBuilder.addProject(
-                rexBuilder.makeCast(
-                    targetType,
-                    programBuilder.makeInputRef(i)),
-                sourceNames[i]);
+            boolean skip = false;
+            switch (section) {
+            case FENNEL:
+                skip = isCustom;
+                break;
+            case JAVA_ONLY:
+                skip = !isCustom;
+                break;
+            }
+
+            RexNode input = programBuilder.makeInputRef(i);
+            RexNode node;
+            if (skip) {
+                // simple projection
+                node = input;
+            } else if (isCustom) {
+                // custom user defined routine
+                node = 
+                    rexBuilder.makeCall(
+                        getUdr(targetType, sourceTypes[i]),
+                        rexBuilder.makeLiteral(getFormat(targetType)),
+                        input);
+            } else {
+                // simple cast
+                node = 
+                    rexBuilder.makeCast(
+                        targetType, 
+                        input);
+            }
+            programBuilder.addProject(node, sourceNames[i]);
         }
-        RexProgram program = programBuilder.getProgram();
-        return program;
-    }
-
-    /**
-     * Relies on a {@link RexToCalcTranslator} to convert a Rex program into a
-     * Fennel calculator program.
-     */
-    protected String toFennelProgram(RexProgram program)
-    {
-        // translate to a fennel calc program
-        RexToCalcTranslator translator =
-            new RexToCalcTranslator(rexBuilder, rel);
-        return translator.generateProgram(
-                program.getInputRowType(),
-                program);
+        return programBuilder.getProgram();
     }
 
     /**
@@ -151,7 +289,6 @@ public class FlatFileProgramWriter
         case SqlTypeName.Symbol_ordinal:
         case SqlTypeName.Varbinary_ordinal:
         default:
-
             // unsupported for flat files
             assert (false) : "Type is unsupported for flat files: "
                 + sqlType.getSqlTypeName();
@@ -159,6 +296,92 @@ public class FlatFileProgramWriter
         RelDataType type =
             typeFactory.createSqlType(SqlTypeName.Varchar, length);
         return typeFactory.createTypeWithNullability(type, true);
+    }
+    
+    /**
+     * Whether a data type has a custom format
+     */
+    private boolean isCustom(
+        RelDataType type)
+    {
+        return getFormat(type) != null;
+    }
+
+    /**
+     * Returns the custom format for a data type
+     */
+    private String getFormat(RelDataType type) 
+    {
+        switch (type.getSqlTypeName().getOrdinal()) {
+        case SqlTypeName.Date_ordinal:
+            return dateFormat;
+        case SqlTypeName.Time_ordinal:
+            return timeFormat;
+        case SqlTypeName.Timestamp_ordinal:
+            return timestampFormat;
+        default:
+            return null;
+        }
+    }
+
+    /**
+     * Gets a function that converts a string to another type, 
+     * according to a format string. Throws an exception if the 
+     * function was not found.
+     * 
+     * @param type the target type
+     * @param charType the type of the string
+     * @return a matching conversion function
+     */
+    private SqlFunction getUdr(
+        RelDataType type,
+        RelDataType charType)
+    {
+        SqlOperatorTable opTable = 
+            stmt.getSqlOperatorTable();
+
+        SqlIdentifier funcName;
+        switch (type.getSqlTypeName().getOrdinal()) {
+        case SqlTypeName.Date_ordinal:
+            funcName = toDateFuncName;
+            break;
+        case SqlTypeName.Time_ordinal:
+            funcName = toTimeFuncName;
+            break;
+        case SqlTypeName.Timestamp_ordinal:
+            funcName = toTimestampFuncName;
+            break;
+        default:
+            funcName = null;
+            Util.permAssert(false, "invalid datetime type");
+        }
+
+        if (datetimeArgs[0] == null) {
+            datetimeArgs[0] = typeFactory.createSqlType(
+                SqlTypeName.Varchar, DATETIME_FORMAT_ARG_LENGTH);
+        }
+        datetimeArgs[1] = charType;
+
+        SqlFunction func = 
+            SqlUtil.lookupRoutine(
+                opTable,
+                funcName,
+                datetimeArgs,
+                null);
+        Util.permAssert(func != null, "datetime routine not found");
+        return func;
+    }
+    
+    /**
+     * Serializes a program into a format directly usable by a Fennel calc 
+     */
+    public String serializeProgram(RexProgram program, RelNode rel)
+    {
+        RexToCalcTranslator translator =
+            new RexToCalcTranslator(rexBuilder, rel);
+        return translator.generateProgram(
+            program.getInputRowType(),
+            program);
     }
 }
 
---
+++ luciddb/src/com/lucidera/luciddb/applib/datetime/StdConvertDateUdf.java
@@ -0,0 +1,112 @@
+/*
+// $Id$
+// Farrago is an extensible data management system.
+// Copyright (C) 2005-2005 LucidEra, Inc.
+// Copyright (C) 2005-2005 The Eigenbase Project
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
+// any later version approved by The Eigenbase Project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+package com.lucidera.luciddb.applib.datetime;
+
+import com.lucidera.luciddb.applib.resource.*;
+
+import net.sf.farrago.runtime.*;
+
+import java.sql.*;
+import java.text.*;
+import java.util.TimeZone;
+
+/**
+ * Date conversion, based on standard Java libraries
+ *
+ * @author John Pham
+ * @version $Id$
+ */
+public class StdConvertDateUdf
+{
+    public static Date char_to_date(String format, String dateString)
+    {
+        return new Date(charToDateHelper(format, dateString));
+    }
+
+    public static Time char_to_time(String format, String timeString)
+    {
+        return new Time(charToDateHelper(format, timeString));
+    }
+
+    public static Timestamp char_to_timestamp(
+        String format, String timestampString)
+    {
+        return new Timestamp(charToDateHelper(format, timestampString));
+    }
+
+    public static String date_to_char(String format, Date d)
+    {
+        DateFormat df = getDateFormat(format);
+        return df.format(d);
+    }
+
+    public static String time_to_char(String format, Time t)
+    {
+        DateFormat df = getDateFormat(format);
+        return df.format(t);
+    }
+
+    public static String timestamp_to_char(String format, Timestamp ts)
+    {
+        DateFormat df = getDateFormat(format);
+        return df.format(ts);
+    }
+
+    /**
+     * Converts a string to a standard Java date, expressed in milliseconds
+     */
+    private static long charToDateHelper(String format, String s)
+    {
+        DateFormat df = getDateFormat(format, true);
+        long ret;
+        try {
+            ret = df.parse(s).getTime();
+        } catch (ParseException ex) {
+            throw ApplibResourceObject.get().InvalidDateString.ex(
+                format, s);
+        }
+        return ret;
+    }
+
+    /**
+     * Gets a date formatter, caching it in the Farrago runtime context
+     */
+    private static DateFormat getDateFormat(String format, boolean gmt)
+    {
+        SimpleDateFormat sdf =
+            (SimpleDateFormat) FarragoUdrRuntime.getContext();
+        if (sdf == null) {
+            sdf = new SimpleDateFormat(format);
+            if (gmt) {
+                sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+            }
+            FarragoUdrRuntime.setContext(sdf);
+        }
+        return sdf;
+    }
+
+    private static DateFormat getDateFormat(String format) 
+    {
+        return getDateFormat(format, false);
+    }
+}
+
+// End StdConvertDateUdf.java
