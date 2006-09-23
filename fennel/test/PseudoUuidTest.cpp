f4ffd19f268f6c9804feaa44e53480a0b8468e3d
Stephan Zuercher <szuercher@lucidera.com>
2006-09-22 19:25:40 -0800

    FENNEL: work around difference in parsing between pre-0.9.4 UUID         and 0.9.4 and later UUID; enable some mistakenly disabled         tests
    [git-p4: depot-paths = "//open/dt/dev/": change = 7744]

--- fennel/test/PseudoUuidTest.cpp
+++ fennel/test/PseudoUuidTest.cpp
@@ -83,21 +83,23 @@ void PseudoUuidTest::testInvalid()
 void PseudoUuidTest::testComparison()
 {
     PseudoUuid uuid1("00010203-0405-0607-0809-0A0B0C0D0E0F");
-//    PseudoUuid uuid2("00010203-0405-0607-0809-0A0B0C0D0E0F");
-//    PseudoUuid uuid3("0F0E0D0C-0B0A-0908-0706-050403020100");
-//
-//    BOOST_CHECK(uuid1 == uuid2);
-//    BOOST_CHECK(uuid1 != uuid2);
-//    BOOST_CHECK(uuid2 != uuid3);
+    PseudoUuid uuid2("00010203-0405-0607-0809-0A0B0C0D0E0F");
+    PseudoUuid uuid3("0F0E0D0C-0B0A-0908-0706-050403020100");
+
+    BOOST_CHECK(uuid1 == uuid2);
+    BOOST_CHECK(uuid1 != uuid3);
+    BOOST_CHECK(uuid2 != uuid3);
 }
 
 void PseudoUuidTest::testParsing()
 {
     PseudoUuid uuid1("03020100-0504-0706-0809-0A0B0C0D0E0F");
 
-    for(int i = 0; i < PseudoUuid::UUID_LENGTH; i++) {
-        BOOST_CHECK_EQUAL(i, uuid1.getByte(i));
-    }
+// Pre version 0.9.4 returns parsed UUID with bytes in the order given above.
+// Post version 0.9.4 returns parsed UUID with bytes in host endian order
+//    for(int i = 0; i < PseudoUuid::UUID_LENGTH; i++) {
+//        BOOST_CHECK_EQUAL(i, uuid1.getByte(i));
+//    }
 
     PseudoUuid uuid2("00000000-0000-0000-0000-000000000000");
 
