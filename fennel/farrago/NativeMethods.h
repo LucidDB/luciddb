27bcdf97b1f785afa0870cb52c7ac06e8ee3e8ca
Zelaine Fong <zelaine@users.sourceforge.net>
2006-10-31 14:38:52 -0800

    FARRAGO/FENNEL - Model changes required for LER-2614.  Splicer exec stream        now takes a parameter indicating whether or not it should ignore        duplicate rid values.  The parameter is set only in the case where        we're inserting into the deletion index on a MERGE.
    [git-p4: depot-paths = "//open/lu/dev/": change = 8034]

--- fennel/farrago/NativeMethods.h
+++ fennel/farrago/NativeMethods.h
@@ -7,7 +7,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-/* Inaccessible static: awtToolkit */
 #undef net_sf_farrago_fennel_FennelStorage_CLOSE_RESULT
 #define net_sf_farrago_fennel_FennelStorage_CLOSE_RESULT 0L
 #undef net_sf_farrago_fennel_FennelStorage_CLOSE_ABORT
