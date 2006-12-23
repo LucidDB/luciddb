ee6f3d39a8dcff6e0fd45fda6d42e3e365ec4a4f
John V. Sichi <jsichi@gmail.com>
2006-12-22 17:02:40 -0800

    INTEGRATION:  manual merge of eigenchange 8364 from        dt/dev to dev
    [git-p4: depot-paths = "//open/dev/": change = 8396]

--- farrago/unitsql/expressions/datatypes.sql
+++ farrago/unitsql/expressions/datatypes.sql
@@ -28,17 +28,33 @@ values TIMESTAMP '2004-12-01 12:01:01';
 select 0e0 from (values (0));
 
 -- Verify that type of character string literals is CHARACTER as it should be
+-- dtbug 401 (JIRA FRG-74)
 
+-- singleline literals
 create view literal_view(lit1,lit2,lit3,lit4,lit5) 
 as values ('pumpkin ','pie','','  ',cast('' as varchar(0)));
 
-select column_name,type_name,column_size
+select column_name,type_name,column_size,nullable
 from sys_boot.jdbc_metadata.columns_view
 where table_name='LITERAL_VIEW'
 order by 1;
 
+-- multiline literals
+create view literal_view2(lit1,lit2,lit3,lit4) 
+as values ('pump'
+'kin ','p'
+'ie',''
+'',' '
+' ');
+
+select column_name,type_name,column_size,nullable
+from sys_boot.jdbc_metadata.columns_view
+where table_name='LITERAL_VIEW2'
+order by 1;
+
 !set outputformat csv
 select * from literal_view;
+select * from literal_view2;
 !set outputformat table
 
 -- FRG-38:  boundary conditions with index on real datatype
