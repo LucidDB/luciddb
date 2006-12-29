e2a8f29b1a2c7dae65bb8f3e550ec67a88cacaf4
Oscar Gothberg <ogothberg@lucidera.com>
2006-12-29 15:46:26 -0800

    LUCIDDB: new udp for quietly deleting schemas. LER-3288
    [git-p4: depot-paths = "//open/lu/dev/": change = 8420]

---
+++ luciddb/src/com/lucidera/luciddb/applib/util/DropSchemaIfExistsUdp.java
@@ -0,0 +1,81 @@
+/*
+// $Id$
+// LucidDB is a DBMS optimized for business intelligence.
+// Copyright (C) 2006-2006 LucidEra, Inc.
+// Copyright (C) 2006-2006 The Eigenbase Project
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
+// any later version approved by The Eigenbase Project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+package com.lucidera.luciddb.applib.util;
+
+import com.lucidera.luciddb.applib.resource.*;
+import org.eigenbase.util.StackWriter;
+import java.sql.*;
+import java.io.*;
+
+
+/**
+ * Tries to drop a schema. Won't return errors neither if successful nor
+ * unsuccessful.
+ *
+ * @param schemaName name of schema to drop.
+ * @param restrictOrCascade either "RESTRICT" or "CASCADE", supplied as a
+ * parameter to the SQL DROP command.
+ *
+ * @author Oscar Gothberg
+ * @version $Id$
+ */
+public abstract class DropSchemaIfExistsUdp {
+    
+    public static void execute(String schemaName, String restrictOrCascade) 
+        throws ApplibException {
+        
+        try {
+            PreparedStatement ps;
+            Statement stmt;
+            ResultSet rs;
+            Connection conn = null;
+            StringWriter sw;
+            StackWriter stackw;
+            PrintWriter pw;
+            
+            // set up a jdbc connection
+            conn = DriverManager.getConnection("jdbc:default:connection");
+            stmt = conn.createStatement();
+            
+            // make sure restrictOrCascade actually says "RESTRICT" or "CASCADE"
+            if (!restrictOrCascade.toUpperCase().equals("RESTRICT") 
+                && !restrictOrCascade.toUpperCase().equals("CASCADE")) {
+                throw ApplibResourceObject.get().ParameterMustBeEitherRestrictOrCascade.ex();
+            }
+            
+            // try to drop the schema
+            sw = new StringWriter();
+            stackw = new StackWriter(sw, StackWriter.INDENT_SPACE4);
+            pw = new PrintWriter(stackw);
+            pw.print("drop schema ");
+            StackWriter.printSqlIdentifier(pw, schemaName);
+            pw.print(" " + restrictOrCascade);
+            pw.close();
+            stmt.executeUpdate(sw.toString());
+
+        } catch (SQLException e) {
+            // just swallow all SQLExceptions, we want this UDP to be quiet.
+            return;
+        }
+    }
+}
+
+// End DropSchemaIfExistsUdp.java
