1c8379e65722c32aba84ec6a894304a38f1f1fd4
John Kalucki <dtepf(@)ignore.kalucki.ignore.com>
2007-06-04 18:49:58 -0800

    BOOTSTRAP: Add nitpick cruise control configuration. Add aspen opto(mized) build. Remove config-aspen.xml script, is now generated by 	config-aspen.generate.sh, and config-aspen.xml.src Add a stop cruise control script.
    [git-p4: depot-paths = "//open/dt/dev/": change = 9409]

--- bootstrap/startcc-chelmsford.sh
+++ bootstrap/startcc-chelmsford.sh
@@ -10,8 +10,16 @@ if [ "!" -d logs/aspen ]; then
     mkdir logs/aspen
 fi
 
+if [ "!" -d logs/aspen-opto ]; then
+    mkdir logs/aspen-opto
+fi
+
 if [ "!" -d artifacts/aspen ]; then
     mkdir artifacts/aspen
+fi
+
+if [ "!" -d artifacts/aspen-opto ]; then
+    mkdir artifacts/aspen-opto
 fi
 
 if [ "!" -d build/aspen ]; then
