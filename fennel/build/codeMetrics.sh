f66d27f9bebd5f1d763d76c216f44613ea1d9571
John V. Sichi <jsichi@gmail.com>
2007-06-30 11:14:00 -0800

    FENNEL:  rough start on code metrics script
    [git-p4: depot-paths = "//open/lu/dev/": change = 9540]

---
+++ fennel/build/codeMetrics.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Gather source code metrics such as lines of code
+# Run this against a pristine source tree (not after a build has polluted it)
+
+cd ${FENNEL_HOME}
+
+echo "Unique file extensions"
+for file in `find . ! -type d -print`;
+do
+  echo ${file##*.}
+done | sort | uniq
+
+echo "*** Line counts for green zone"
+wc -l `find . \( -name '*.cpp' -o -name '*.h' -o -name '*.ypp' -o -name '*.lpp' \) -a ! -wholename '*disruptivetech*' -a ! -wholename '*lucidera*'`
+
+echo "*** Line counts for Disruptive Tech yellow zone"
+wc -l `find disruptivetech -name '*.cpp' -o -name '*.h'`
+
+echo "*** Line counts for LucidEra yellow zone"
+wc -l `find lucidera -name '*.cpp' -o -name '*.h'`
