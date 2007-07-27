1d22d5ceb01d34fa63e77a2b8f540b36b7ae7a5e
Jack Frost <jackf0(@)ignore.aol.com>
2007-07-27 07:22:30 -0800

    Farrago: Fixes USING conditional for JOIN statement.  Fixes dt294. Problem was in SQL to REL conversion.  USING parameters were being treated as general expressions.
    [git-p4: depot-paths = "//open/dt/dev/": change = 9661]

---
+++ farrago/unitsql/optimizer/joinUsing.sql
@@ -0,0 +1,14 @@
+-- $Id$
+-- Testing the USING clause of the JOIN statement.
+
+set schema 'sales';
+
+-- USING is syntactic candy for an ON condition.  Plan and output for the two
+-- queries should be the same.
+
+explain plan for select * from emps join depts on emps.deptno = depts.deptno;
+explain plan for select * from emps join depts using (deptno);
+
+
+select * from emps join depts on emps.deptno = depts.deptno;
+select * from emps join depts using (deptno);
