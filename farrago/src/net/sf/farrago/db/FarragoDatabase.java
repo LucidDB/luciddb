23b5ef28a35fb90134d55699d9df3c9f45e9503b
John V. Sichi <jsichi@gmail.com>
2007-08-14 14:22:40 -0800

    FARRAGO:  fix for FRG-99 (and LER-5798):  catch exceptions thrown from timers and convert them into task-level cancellations
    [git-p4: depot-paths = "//open/lu/dev/": change = 9757]

--- farrago/src/net/sf/farrago/db/FarragoDatabase.java
+++ farrago/src/net/sf/farrago/db/FarragoDatabase.java
@@ -1090,17 +1090,23 @@ public class FarragoDatabase
      * trace configuration file whenever it changes.
      */
     private class WatchdogTask
-        extends TimerTask
+        extends FarragoTimerTask
     {
         private long prevTraceConfigTimestamp;
 
         WatchdogTask()
         {
+            super(tracer);
             prevTraceConfigTimestamp = traceConfigFile.lastModified();
         }
 
-        // implement Runnable
-        public void run()
+        // implement FarragoTimerTask
+        protected void runTimer()
+        {
+            checkTraceConfig();
+        }
+
+        private void checkTraceConfig()
         {
             long traceConfigTimestamp = traceConfigFile.lastModified();
             if (traceConfigTimestamp == 0) {
@@ -1124,10 +1130,15 @@ public class FarragoDatabase
     }
 
     private class CheckpointTask
-        extends TimerTask
+        extends FarragoTimerTask
     {
-        // implement Runnable
-        public void run()
+        CheckpointTask()
+        {
+            super(tracer);
+        }
+        
+        // implement FarragoTimerTask
+        public void runTimer()
         {
             requestCheckpoint(true, true);
         }
