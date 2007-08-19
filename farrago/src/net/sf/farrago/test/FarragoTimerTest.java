ed9440c4a970bee180417a4982c13cfe0a4666c9
John V. Sichi <jsichi@gmail.com>
2007-08-19 01:39:57 -0800

    INTEGRATION: Integration of 9757 from child to parent (branch spec: dev_to_lu_dev).    ==> 9757, Date:	2007/08/14 15:22:40        User:	jvs        Description:        	FARRAGO:  fix for FRG-99 (and LER-5798):  catch exceptions        	thrown from timers and convert them into task-level cancellations
    [git-p4: depot-paths = "//open/dev/": change = 9787]

---
+++ farrago/src/net/sf/farrago/test/FarragoTimerTest.java
@@ -0,0 +1,121 @@
+/*
+// $Id$
+// Farrago is an extensible data management system.
+// Copyright (C) 2007-2007 The Eigenbase Project
+// Copyright (C) 2007-2007 Disruptive Tech
+// Copyright (C) 2007-2007 LucidEra, Inc.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
+// any later version approved by The Eigenbase Project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+package net.sf.farrago.test;
+
+import net.sf.farrago.util.*;
+
+import org.eigenbase.util.*;
+
+import junit.framework.*;
+
+import java.util.*;
+import java.util.logging.*;
+
+/**
+ * FarragoTimerTest tests {@link FarragoTimerAllocation} and
+ * {@link FarragoTimerTask}.
+ *
+ * @author John Sichi
+ * @version $Id$
+ */
+public class FarragoTimerTest extends TestCase
+{
+    int nTicks;
+    
+    /**
+     * Creates a new FarragoTimerTest object.
+     */
+    public FarragoTimerTest(String testName)
+        throws Exception
+    {
+        super(testName);
+    }
+
+    public void testGoodTask()
+    {
+        executeTask(new GoodTask());
+
+        // Don't know exactly how many ticks will fit in; verify
+        // that it's at least two.
+        assertTrue(nTicks > 1);
+    }
+
+    public void testBadTask()
+    {
+        executeTask(new BadTask());
+        
+        // Verify that timer stopped ticking after burp.
+        assertEquals(3, nTicks);
+    }
+
+    private void executeTask(FarragoTimerTask task)
+    {
+        FarragoCompoundAllocation owner = new FarragoCompoundAllocation();
+        try {
+            nTicks = 0;
+            Timer timer = new Timer("FarragoTimerTest");
+            new FarragoTimerAllocation(owner, timer);
+            timer.schedule(
+                task,
+                50,
+                50);
+            Thread.currentThread().sleep(1000);
+        } catch (InterruptedException ex) {
+            throw Util.newInternal(ex);
+        } finally {
+            owner.closeAllocation();
+        }
+    }
+
+    private class GoodTask extends FarragoTimerTask
+    {
+        GoodTask()
+        {
+            super(null);
+        }
+
+        // implement FarragoTimerTask
+        protected void runTimer()
+        {
+            ++nTicks;
+        }
+    }
+
+    private class BadTask extends FarragoTimerTask
+    {
+        BadTask()
+        {
+            super(null);
+        }
+
+        // implement FarragoTimerTask
+        public void runTimer()
+        {
+            ++nTicks;
+            if (nTicks == 3) {
+                throw new RuntimeException("burp");
+            }
+        }
+    }
+}
+
+// End FarragoTimerTest.java
---
+++ farrago/src/net/sf/farrago/util/FarragoTimerTask.java
@@ -0,0 +1,80 @@
+/*
+// $Id$
+// Farrago is an extensible data management system.
+// Copyright (C) 2007-2007 The Eigenbase Project
+// Copyright (C) 2007-2007 Disruptive Tech
+// Copyright (C) 2007-2007 LucidEra, Inc.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
+// any later version approved by The Eigenbase Project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+package net.sf.farrago.util;
+
+import java.util.*;
+import java.util.logging.*;
+
+/**
+ * FarragoTimerTask refines {@link TimerTask} to guarantee safety based on the
+ * way Farrago runs timers (see {@link FarragoTimerAllocation}).
+ *
+ *<p>
+ *
+ * TODO jvs 13-Aug-2007:  add a facility for subclasses to be able to
+ * distinguish fatal exceptions from recoverable ones, so that
+ * timers can be allowed to keep running after an exception.
+ *
+ * @author John Sichi
+ * @version $Id$
+ */
+public abstract class FarragoTimerTask extends TimerTask
+{
+    private final Logger tracer;
+
+    /**
+     * Creates a new FarragoTimerTask.
+     *
+     * @param tracer logger on which to trace exceptions
+     */
+    protected FarragoTimerTask(Logger tracer)
+    {
+        this.tracer = tracer;
+    }
+
+    // implement Runnable
+    public final void run()
+    {
+        try {
+            runTimer();
+        } catch (Throwable ex) {
+            // NOTE jvs 13-Aug-2007: Do not propagate the exception, since that
+            // would kill other tasks running on the same timer, and worse,
+            // cause shutdown errors or hangs when we attempt to cancel the
+            // timer.  (See http://issues.eigenbase.org/browse/FRG-99.)
+            // Instead, just log the exception's stack and cancel the task (not
+            // the timer).  In the future, call subclass to decide on
+            // recoverability; if recoverable, don't cancel.
+            if (tracer != null) {
+                tracer.log(Level.SEVERE, "FarragoTimerTask failed", ex);
+            }
+            cancel();
+        }
+    }
+
+    /**
+     * Runs the timer action as specified by subclass.
+     */
+    protected abstract void runTimer();
+}
+
+// End FarragoTimerTask.java
