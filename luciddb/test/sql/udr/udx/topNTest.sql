b888494dca2c587cc9a34588fca990c840868b26
jpalmert <jpalmert@lucidera.com>
2008-04-10 15:11:29 -0800

    LUCIDDB/QA: Updated unit test for applib.topn
    [git-p4: depot-paths = "//open/lu/dev/": change = 10857]

--- luciddb/test/sql/udr/udx/topNTest.sql
+++ luciddb/test/sql/udr/udx/topNTest.sql
@@ -4,28 +4,33 @@
 create schema TOPNTEST;
 set schema 'TOPNTEST';
 create table T1 (COL1 integer, COL2 varchar(255));
-insert into T1 (COL1, COL2) values (10, 'aa');
+insert into T1 (COL1, COL2) values (null, 'aa');
 insert into T1 (COL1, COL2) values (20, null);
 insert into T1 (COL1, COL2) values (30, 'cc');
 insert into T1 (COL1, COL2) values (40, 'dd');
-insert into T1 (COL1, COL2) values (null, 'ee');
+insert into T1 (COL1, COL2) values (50, 'ee');
 
 -- tests
 select * from table(
-  applib.topn( cursor(select * from T1), -1 )
-);
+  applib.topn( cursor(select * from T1 order by 1), -1 )
+)
+order by 1;
 select * from table(
-  applib.topn( cursor(select * from T1), 0 )
-);
+  applib.topn( cursor(select * from T1 order by 1), 0 )
+)
+order by 1;
 select * from table(
-  applib.topn( cursor(select * from T1), 4 )
-);
+  applib.topn( cursor(select * from T1 order by 1), 4 )
+)
+order by 1;
 select * from table(
-  applib.topn( cursor(select * from T1), 5 )
-);
+  applib.topn( cursor(select * from T1 order by 1), 5 )
+)
+order by 1;
 select * from table(
-  applib.topn( cursor(select * from T1), 6 )
-);
+  applib.topn( cursor(select * from T1 order by 1), 6 )
+)
+order by 1;
 
 -- tear down
 drop schema TOPNTEST cascade;
