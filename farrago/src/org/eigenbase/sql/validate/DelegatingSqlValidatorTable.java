c2b3b1cc5531e9f562f23743c6822fc449a57ff3
Julian Hyde <julianhyde(@)ignore.gmail.com>
2009-03-26 23:20:01 -0800

    FARRAGO: Oops, fix preambles.
    [git-p4: depot-paths = "//open/dt/dev/": change = 12527]

--- farrago/src/org/eigenbase/sql/validate/DelegatingSqlValidatorTable.java
+++ farrago/src/org/eigenbase/sql/validate/DelegatingSqlValidatorTable.java
@@ -2,7 +2,7 @@
 // $Id$
 // Package org.eigenbase is a class library of data management components.
 // Copyright (C) 2009-2009 The Eigenbase Project
-// Copyright (C) 2009-2009 Disruptive Tech
+// Copyright (C) 2009-2009 SQLstream, Inc.
 // Copyright (C) 2009-2009 LucidEra, Inc.
 //
 // This program is free software; you can redistribute it and/or modify it
