2f8bf4e7c765ee390c0b10ae50159d57f36dfdfb
Hunter Payne <hunter.payne(@)ignore.sqlstream.com>
2009-12-13 12:45:07 -0800

    FARRAGO/FENNEL: upgrading boost to 1.40 and upgrading the lockfree and task libraries.
    [git-p4: depot-paths = "//open/dt/dev/": change = 13228]

--- fennel/build/buildBoostLibs.sh
+++ fennel/build/buildBoostLibs.sh
@@ -7,16 +7,23 @@ set -e
 set -v
 
 cd ${BOOST_DIR}
-./configure --prefix=${BOOST_DIR} \
-    --with-libraries=date_time,filesystem,regex,system,thread,task,test \
-    --without-icu
-echo "using stlport : 5.1.6 : ${STLPORT_LOCATION}/stlport ${STLPORT_LOCATION}/lib ;" >> ${BOOST_DIR}/user-config.jam
-echo "stage: .dummy" >> ${BOOST_DIR}/Makefile
-echo '	@$(BJAM) $(BJAM_CONFIG) --user-config=user-config.jam --prefix=$(prefix) --exec-prefix=$(exec_prefix) --stagedir=$(prefix) $(LIBS) stage || echo "Not all Boost libraries built properly."' >> ${BOOST_DIR}/Makefile
+if test "${TARGET_OS}" = "windows"
+then
+    ./bootstrap.bat --prefix=${BOOST_DIR} \
+        --with-libraries=date_time,filesystem,regex,system,thread,task,test \
+        --without-icu
+else
+    ./bootstrap.sh --prefix=${BOOST_DIR} \
+        --with-libraries=date_time,filesystem,regex,system,thread,task,test \
+        --without-icu
+fi
+echo "using stlport : 5.1.6 : ${STLPORT_LOCATION}/stlport : ${STLPORT_LOCATION}/lib ;" >> ${BOOST_DIR}/tools/build/v2/user-config.jam
+#echo "stage: .dummy" >> ${BOOST_DIR}/Makefile
+#echo '	@$(BJAM) $(BJAM_CONFIG) --user-config=user-config.jam --prefix=$(prefix) --exec-prefix=$(exec_prefix) --stagedir=$(prefix) $(LIBS) stage || echo "Not all Boost libraries built properly."' >> ${BOOST_DIR}/Makefile
 
 if test "${TARGET_OS}" = "linux-gnu"
 then
-    make BJAM_CONFIG="toolset=${BOOST_TOOLSET} variant=debug,release link=shared runtime-link=shared stdlib=stlport --layout=system" stage
+    bjam toolset=${BOOST_TOOLSET} stdlib=stlport variant=debug,release link=shared runtime-link=shared threading=multi address-model=${CPU_BITS} --layout=tagged --stagedir=${BOOST_DIR} cflags=-Wno-long-long
 else
-    make BJAM_CONFIG="toolset=${BOOST_TOOLSET} target-os=windows threadapi=win32 variant=debug,release link=shared runtime-link=shared stdlib=stlport --layout=system" stage
+    bjam toolset=${BOOST_TOOLSET} stdlib=stlport target-os=windows threadapi=win32 variant=debug,release link=shared runtime-link=shared threading=multi address-model=${CPU_BITS} --layout=tagged  --stagedir=${BOOST_DIR}
 fi
