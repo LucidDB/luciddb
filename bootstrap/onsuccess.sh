d85a934813d7cc399e9d6d2c6aa97701b21a29e7
aspen <jhyde(@)ignore.users.sourceforge.net>
2009-12-22 21:29:06 -0800

    BOOTSTRAP/ASPEN: add link and MD5 for client tools.
    [git-p4: depot-paths = "//open/dt/dev/": change = 13265]

--- bootstrap/onsuccess.sh
+++ bootstrap/onsuccess.sh
@@ -2,12 +2,15 @@
 BUILDNAME=$1
 LINKNAME=$2
 LINKINFO=$LINKNAME-info.txt
+LINKCLIENT=$LINKNAME-client
 MD5INFO=$LINKNAME-md5.txt
 
 # called by CruiseControl with current directory set to ~/work
 cd artifacts/$BUILDNAME
 ANS=`find -name "SQLstream-*.bin" | sort -r | head -n 1`
 INSTALLER=${ANS:2}
+ANS=`find -name "SQLstream*ClientTools.zip" | sort -r | head -n 1`
+CLIENTTOOLS=${ANS:2}
 
 # CC artifactspublisher copies stale link, info, and md5 to ~/web/artifacts. Delete them.
 rm -f ~/web/artifacts/$BUILDNAME/$LINKNAME*
@@ -15,16 +18,17 @@ rm -f ~/web/artifacts/$BUILDNAME/$LINKNAME*
 # CC artifactspublisher drops permissions. Make timestamped version executable.
 chmod 755 $INSTALLER
 
-# create link to installer and text file with the link target 
+# create link to installer, client tools, and text file with the link target 
 # so scp clients can determine what they've downloaded
 rm -f $LINKINFO
 ln -sf $INSTALLER $LINKNAME
+ln -sf $CLIENTTOOLS $LINKCLIENT
 echo $INSTALLER >$LINKINFO
 
 # create MD5 signature file
 rm -f $MD5INFO
 DIR=$PWD
-(cd `dirname $INSTALLER`; md5sum `basename $INSTALLER` >$DIR/$MD5INFO)
+(cd `dirname $INSTALLER`; md5sum `basename $INSTALLER` `basename $CLIENTTOOLS` >$DIR/$MD5INFO)
 
 # debugging info
 #echo ----- $BUILDNAME ----- >>~/work/onsuccess.out.txt
