9a16ced6a4c7192dbb8b99bd6a6aa1034594cd2c
John V. Sichi <jsichi@gmail.com>
2010-07-03 16:12:31 -0800

    EXTENSIONS:  refine SFDC file header description, and change license from GPL to LGPL
    [git-p4: depot-paths = "//open/dy/dev/": change = 13733]

--- extensions/conn/sfdc/src/net/sf/farrago/namespace/sfdc/resource/SfdcResourceObject.java
+++ extensions/conn/sfdc/src/net/sf/farrago/namespace/sfdc/resource/SfdcResourceObject.java
@@ -1,6 +1,23 @@
 /*
 // $Id$
-// (C) Copyright 2006-2006 DynamoBI Corporation
+// SFDC Connector is an Eigenbase SQL/MED connector for Salesforce.com
+// Copyright (C) 2010 The Eigenbase Project
+// Copyright (C) 2010 SQLstream, Inc.
+// Copyright (C) 2010 DynamoBI Corporation
+//
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or (at
+// your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 package net.sf.farrago.namespace.sfdc.resource;
 
