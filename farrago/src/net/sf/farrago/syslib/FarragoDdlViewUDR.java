38c2c6d789663c4c93e7e27f3deb4eac8fc143d3
Kevin Secretan <ksecretan@dynamobi.com>
2010-11-26 19:18:12 -0800

    FARRAGO: FRG-405 Woops, forgot proper file preambles.
    [git-p4: depot-paths = "//open/dy/dev/": change = 13939]

--- farrago/src/net/sf/farrago/syslib/FarragoDdlViewUDR.java
+++ farrago/src/net/sf/farrago/syslib/FarragoDdlViewUDR.java
@@ -2,23 +2,23 @@
 // $Id$
 // Farrago is an extensible data management system.
 // Copyright (C) 2010 The Eigenbase Project
-// Copyright (C) 2010 DynamoBI Corporation
+// Copyright (C) 2010 SQLstream, Inc.
+// Copyright (C) 2010 Dynamo BI Corporation
 //
-// This library is free software; you can redistribute it and/or modify it
-// under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation; either version 2.1 of the License, or (at
-// your option) any later version.
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 2 of the License, or (at your option)
+// any later version approved by The Eigenbase Project.
 //
-// This library is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
+// GNU General Public License for more details.
 //
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
-
 package net.sf.farrago.syslib;
 
 import java.sql.*;
